4b697675de26da5ddf62fbfbf4aa0bedca2caa56
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-12-03 12:35:40 -0300

    feat: change throw error logic to use next

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -2,8 +2,8 @@ import * as songService from '../services/songServices.js';
 import { isSongInputValid } from '../validations/songValidation.js';
 import SongError from '../errors/SongError';
 
-const newSong = async (req, res) => {
-  if (!isSongInputValid) return res.sendStatus(400);
+const newSong = async (req, res, next) => {
+  if (!isSongInputValid(req.body)) return res.sendStatus(400);
 
   const { name, youtubeLink } = req.body;
 
@@ -12,7 +12,7 @@ const newSong = async (req, res) => {
     return res.status(201).send(song);
   } catch (error) {
     if (error instanceof SongError) return res.status(409).send(error.message);
-    throw error;
+    next(error);
   }
 };
 
