3a30e023efc3cdfb3f9945d918fd06e01d4189b8
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-12-04 16:41:27 -0300

    feat: change controller vote function to identify vote type

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -16,11 +16,12 @@ const newSong = async (req, res, next) => {
   }
 };
 
-const newUpVote = async (req, res, next) => {
+const newVote = async (req, res, next) => {
   const { id } = req.params;
+  const { voteType } = req.path.slice(req.path.lastIndexOf('/') + 1);
 
   try {
-    const updatedSong = await songService.upvoteASong({ id });
+    const updatedSong = await songService.upvoteASong({ id, voteType });
     return res.status(200).send(updatedSong);
   } catch (error) {
     if (error instanceof SongError) return res.status(404).send(error.message);
@@ -28,4 +29,4 @@ const newUpVote = async (req, res, next) => {
   }
 };
 
-export { newSong, newUpVote };
+export { newSong, newVote };
