9e48ce767b5303b96184757c7a1294605612dd6c
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-12-04 02:17:41 -0300

    test: throw error when song link already exists

--- tests/unit/songServices.test.js
+++ tests/unit/songServices.test.js
@@ -3,13 +3,27 @@ import * as songRepository from '../../src/repositories/songRepository.js';
 import SongError from '../../src/errors/SongError.js';
 
 describe('Song Service', () => {
-  test('Should throw a SongError', async () => {
+  test('Should throw a SongError when name is already in use', async () => {
     jest
       .spyOn(songRepository, 'findSongByName')
       .mockImplementationOnce(() => 1);
 
     const name = 'test';
     const promise = songService.createSong({ name });
+    await expect(promise).rejects.toThrowError(SongError);
+  });
+
+  test('Should throw a SongError when link is already in use', async () => {
+    jest
+      .spyOn(songRepository, 'findSongByName')
+      .mockImplementationOnce(() => 0);
+
+    jest
+      .spyOn(songRepository, 'findSongByLink')
+      .mockImplementationOnce(() => 1);
+
+    const youtubeLink = 'test';
+    const promise = songService.createSong({ youtubeLink });
     await expect(promise).rejects.toThrowError(SongError);
   });
 });
