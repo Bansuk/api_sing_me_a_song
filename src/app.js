7691d5422b5e2805d74839f80ee5a379f2dd7f88
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-12-06 14:55:08 -0300

    Realocate first part of the URL to the app file

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(recommendationRouter);
+app.use('/recommendations', recommendationRouter);
 
 app.use(serverMiddlewareError);
 
--- src/repositories/songRepository.js
+++ src/repositories/songRepository.js
@@ -2,7 +2,7 @@ import connection from '../database/database.js';
 
 const insertSong = async ({ name, youtubeLink, score }) => {
   const result = await connection.query(
-    'INSERT INTO song (name, link, score) VALUES ($1, $2, $3) RETURNING *;',
+    'INSERT INTO song (name, "youtubeLink", score) VALUES ($1, $2, $3) RETURNING *;',
     [name, youtubeLink, score],
   );
 
@@ -18,9 +18,10 @@ const findSongByName = async ({ name }) => {
 };
 
 const findSongByLink = async ({ youtubeLink }) => {
-  const result = await connection.query('SELECT * FROM song WHERE link = $1;', [
-    youtubeLink,
-  ]);
+  const result = await connection.query(
+    'SELECT * FROM song WHERE "youtubeLink" = $1;',
+    [youtubeLink],
+  );
 
   return result.rowCount;
 };
