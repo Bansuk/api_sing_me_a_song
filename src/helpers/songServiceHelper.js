89e0c90ae9350bb2c8c4d5fb6d11ec92beca69e1
ligia.bozzi@gmail.com <ligia@respondeai.com.br>
2021-12-08 11:07:52 -0300

    preparing review

--- src/helpers/songServiceHelper.js
+++
@@ -1,9 +0,0 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-export { getRandomInt, getRandomArbitrary };
